b6533caf42ea5c04c95420bb6e55da41e98c5091
Yousef Medhat <148549942+yousefmedhat00@users.noreply.github.com>
2024-01-30 14:05:55 +0200

    Create function to get player's choice

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const selection = ["rock","paper","scissors"]
+let computerScore = 0;
+let playerScore = 0;
 
 function getComputerChoice() {
-    const computerChoice = selection.[Math.floor(Math.random()*selection.length)];
-    return computerChoice;
+    const computerSelection = selection[Math.floor(Math.random()*selection.length)];
+    return computerSelection;
 }
+
+function getPlayerSelection() {
+    const playerSelection = prompt("Rock, Paper of Scissors?").toLowerCase();
+    return playerSelection;
+}
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        alert("It's a tie");
+     } else if(computerSelection === 'rock' && playerSelection === 'scissors') {
+        alert('Computer wins.')
+
+
+     } 
+    }
+
+const playerSelection = getPlayerSelection();
+const computerSelection = getComputerChoice();
+console.log(computerSelection)
+console.log(playerSelection)
