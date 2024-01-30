e2162c43b06dce2607d4e91dce9da2f8666821fe
Yousef Medhat <148549942+yousefmedhat00@users.noreply.github.com>
2024-01-30 12:48:20 +0200

    Create function that makes the computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const selection = ["rock","paper","scissors"]
+
+function getComputerChoice() {
+    const computerChoice = selection.[Math.floor(Math.random()*selection.length)];
+    return computerChoice;
+}
