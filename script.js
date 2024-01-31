a14b39122908aaf353b14cd9296e5c10d226b812
Yousef Medhat <148549942+yousefmedhat00@users.noreply.github.com>
2024-01-31 12:35:59 +0200

    Create function that plays a game of five rounds and shows if player won or lost and shows their score

--- script.js
+++ script.js
@@ -10,39 +10,54 @@ function getPlayerSelection() {
     const playerSelection = prompt("Rock, Paper of Scissors?").toLowerCase();
     return playerSelection;
 }
+
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
         alert("It's a tie");
+        return "It's a tie";
      } else if(computerSelection === "rock" && playerSelection === "scissors") {
         alert("You lose! Rock beats Scissors");
         computerScore++;
+        return "You lose! Rock beats Scissors";
      } else if(computerSelection === "rock" && playerSelection === "paper"){
         alert("You win! Paper beats Rock");
         playerScore++
+        return "You win! Paper beats Rock";
      } else if(computerSelection === "scissors" && playerSelection === "paper"){
         alert("You lose! Scissors beat Paper");
         computerScore++;
+        return "You lose! Scissors beat Paper";
      } else if(computerSelection === "scissors" && playerSelection === "rock"){
         alert("You win! Rock beats Scissors");
         playerScore++;
+        return "You win! Rock beats Scissors";
      } else if(computerSelection === "paper" && playerSelection === "rock"){
         alert("You lose! Paper beats Rock");
         computerScore++;
+        return "You lose! Paper beats Rock";
      } else {
         alert("You win! Scissors bears Paper");
         playerScore++;
+        return "You win! Scissors bears Paper";
      }
 }
-
 function playGame(){
-    playRound(playerSelection, computerSelection);
-    playRound(playerSelection, computerSelection);
-    playRound(playerSelection, computerSelection);
-    playRound(playerSelection, computerSelection);
-    playRound(playerSelection, computerSelection);
-    alert(`Your score: ${playerScore}`);
+    for (let i = 0; i < 5; i++){
+        const playerSelection = getPlayerSelection();
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    }
+    console.log(playerScore);
+    console.log(computerScore);
+    if (playerScore < computerScore){
+        alert(`You Lose :(  Your score: ${playerScore}`);
+        let result = `You Lose! :( Your score: ${playerScore}`;
+    } else {
+        alert(`You Win! :)  Your score: ${playerScore}`);
+        let result = `You win! :)  Your score: ${playerScore}`;
+    }
+    return result;
 }
 
 playGame();
-console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection));
+console.log(playGame());
