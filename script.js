42d9326cc2658834e72150277e6662c1c43a2e8a
Yousef Medhat <148549942+yousefmedhat00@users.noreply.github.com>
2024-02-05 11:51:25 +0200

    Add overlay constants

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ const scissorsBtn = document.querySelector("#scissors");
 const result = document.querySelector("#result");
 const playerScoreCard = document.querySelector("#playerScore");
 const computerScoreCard = document.querySelector("#computerScore");
+const playerChoiceCard = document.querySelector("#playerChoice");
+const computerChoiceCard = document.querySelector("#computerChoice");
+
+const overlay = document.createElement("div");
+overlay.classList.add("overlay");
+const overlayCard = document.createElement("div");
+const overlayHeading = document.createElement("div");
+const overlayHeading2 = document.createElement("div");
+const overlayHeadText = document.createElement("h1");
+const overlayHead2Text = document.createElement("h2");
+const startBtn = document.createElement("button");
 
 
 let playerScore = 0;
@@ -14,23 +25,50 @@ let computerScore = 0;
 
 rockBtn.addEventListener('click', () => {
     let computerSelection = getComputerChoice();
+    playerChoiceCard.textContent = "Your Choice: ROCK";
+    computerChoiceCard.textContent = `Computer's Choice: ${computerSelection.toUpperCase()}`;
     result.textContent = playRound("rock", computerSelection);
     if (computerSelection === "scissors") {
         playerScore += 1;
         playerScoreCard.textContent = `Your Score: ${playerScore}`;
+    } else if (computerSelection === "paper"){
+        computerScore += 1;
+        computerScoreCard.textContent = `Computer's Score: ${computerScore}`;
     }
-}
-)
+});
+
+paperBtn.addEventListener('click', () => {
+    let computerSelection = getComputerChoice();
+    playerChoiceCard.textContent = "Your Choice: PAPER";
+    computerChoiceCard.textContent = `Computer's Choice: ${computerSelection.toUpperCase()}`;
+    result.textContent = playRound("paper", computerSelection);
+    if (computerSelection === "rock") {
+        playerScore += 1;
+        playerScoreCard.textContent = `Your Score: ${playerScore}`;
+    } else if (computerSelection === "scissors"){
+        computerScore += 1;
+        computerScoreCard.textContent = `Computer's Score: ${computerScore}`;
+    }
+});
+
+scissorsBtn.addEventListener('click', () => {
+    let computerSelection = getComputerChoice();
+    playerChoiceCard.textContent = "Your Choice: SCISSORS";
+    computerChoiceCard.textContent = `Computer's Choice: ${computerSelection.toUpperCase()}`;
+    result.textContent = playRound("scissors", computerSelection);
+    if (computerSelection === "paper") {
+        playerScore += 1;
+        playerScoreCard.textContent = `Your Score: ${playerScore}`;
+    } else if (computerSelection === "rock"){
+        computerScore += 1;
+        computerScoreCard.textContent = `Computer's Score: ${computerScore}`;
+    }
+});
 
 // function to get a random selection for computer
 function getComputerChoice() {
     const computerSelection = selection[Math.floor(Math.random()*selection.length)];
     return computerSelection;
-}
-// function to get selection from user, converted to lower-case for case insensitivity 
-function getPlayerSelection() {
-    const playerSelection = prompt("Rock, Paper of Scissors?").toLowerCase();
-    return playerSelection;
 }
 
 function playRound(playerSelection, computerSelection){
@@ -50,7 +88,20 @@ function playRound(playerSelection, computerSelection){
      } else {
         return "You win! Scissors bears Paper";
      }
-}
+};
+
+function checkScore(playerScore, computerScore){
+    if (playerScore === 5 || computerScore === 5) { 
+        if (playerScore > computerScore) {
+
+        } else {
+
+        };
+    } else {
+
+    };
+};
+
 function playGame(){
     // keep score
     let computerScore = 0;
